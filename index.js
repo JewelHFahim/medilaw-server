b0ea8cabef748f256b0f25db709038ce0b058ae4
JewelHFahim <jewelhfahim@gmail.com>
2022-11-10 02:09:49 +0600

    added

--- index.js
+++ index.js
@@ -81,6 +81,14 @@ async function run(){
             res.send(review)
         })
 
+        app.delete('/personalreview/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
         // Review Section
 
     }
