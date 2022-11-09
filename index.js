0c8680fc41b590ef6aeffd530cc82a4568e93600
JewelHFahim <jewelhfahim@gmail.com>
2022-11-09 19:53:42 +0600

    review added

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 
+// const uri = "mongodb://localhost:27017";
 const uri = `mongodb+srv://${process.env.ML_USER}:${process.env.ML_PASSWORD}@cluster0.qez1k8e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         const serviceCollection = client.db('mediLaw').collection('services');
+        const userCollection = client.db('mediLaw').collection('users');
+        const reviewCollection = client.db('mediLaw').collection('review');
 
+        // Service Section Start
         app.get('/services', async(req, res)=>{
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -28,6 +32,56 @@ async function run(){
             const result = await serviceCollection.findOne(query);
             res.send(result);
         })
+        // Service Section End
+
+        // Users Section
+        app.post('/users', async(req, res)=>{
+            const users = req.body;
+            const result = await userCollection.insertOne(users);
+            res.send(result);
+            console.log(result);
+        })
+        app.get('/users', async(req, res)=>{
+
+            const query = {};
+            const cursor = userCollection.find(query);
+            const result =  await cursor.toArray();
+            res.send(result);
+        })
+        // USer Section End
+
+
+        // Review Section
+        app.post('/review', async(req, res)=>{
+            const service_id = req.params.id;
+            const query = {_id: ObjectId(service_id)}
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+            console.log(result);
+        })
+
+        app.get('/review', async(req, res)=>{
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const result =  await cursor.toArray();
+            res.send(result);
+        })
+
+          app.get('/personalreview', async(req, res)=>{
+            const id = req.params.id;
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review)
+        })
+
+        // Review Section
 
     }
     finally{
